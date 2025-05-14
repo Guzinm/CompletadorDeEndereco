09595d0b29b93507484a3f23e3c4492eaa5af97f
Guzinh <guslessafim@gmail.com>
2025-05-13 22:07:48 -0300

    Adições finais

---
+++ README.md
@@ -0,0 +1,24 @@
+# CompletadorDeLugar
+
+## Descrição
+CompletadorDeLugar é um projeto desenvolvido para demonstrar conceitos de API. Esta ferramenta tem como objetivo o uso das ferramentas fetchAPI, viaCEP, webStorageAPI.
+
+
+
+## Funcionalidades
+- Completar automaticamente outros campos do formulário de endereço utilizando a API ViaCEP.
+- Alterar o tema do site entre opções disponíveis.
+- Utilizar a Web Storage API para armazenar o tema atual e os dados dos outros campos do formulário.
+
+![Tema Escuro](PrintDark.png)
+![Tema Escuro](PrintLight.png)
+
+## Instalação
+1. Clone o repositório:
+    ```bash
+    git clone https://github.com/seu-usuario/CompletadorDeLugar.git
+    ```
+2. Navegue até o diretório do projeto:
+    ```bash
+    cd CompletadorDeLugar
+    ```
--- script.js
+++ script.js
@@ -1,28 +1,67 @@
+const AllInputs = document.querySelectorAll("input");
+
+function salvarDadosFormulario() {
+
+    const CEP = document.getElementById('CEP').value;
+    const logradouro = document.getElementById('Logradouro').value;
+    const bairro = document.getElementById('Bairro').value;
+    const localidade = document.getElementById('Cidade').value;
+    const uf = document.getElementById('Estado').value;
+
+    localStorage.setItem("endereco", [CEP, logradouro, bairro, localidade, uf]);
+};
+
+function carregarDadosFormulario(enderecoCarregado) {
+
+    const DadosFormatado = enderecoCarregado.split(",")
+
+    document.getElementById('CEP').value = DadosFormatado[0];
+    document.getElementById('Logradouro').value = DadosFormatado[1];
+    document.getElementById('Bairro').value = DadosFormatado[2];
+    document.getElementById('Cidade').value = DadosFormatado[3];
+    document.getElementById('Estado').value = DadosFormatado[4];
+}
+
 document.getElementById("CEP").addEventListener("blur", (evento) => {
 
     const CEP = evento.target.value
     
     if (!(CEP.length == 8)) {
         return;
-    }
+    };
 
     fetch(`https://viacep.com.br/ws/${CEP}/json/`)
-        .then(response => response.json())
+        .then(response => {
+            
+            if (!response.ok) {
+                throw new Error('response not ok');
+            };
+            return response.json();
+
+        })
         .then(data =>{
 
             if(!data.error){
-                document.getElementById('Logradouro').value = data.logradouro
-                document.getElementById('Bairro').value = data.bairro
-                document.getElementById('Cidade').value = data.localidade
-                document.getElementById('Estado').value = data.uf
+                document.getElementById('Logradouro').value = data.logradouro;
+                document.getElementById('Bairro').value = data.bairro;
+                document.getElementById('Cidade').value = data.localidade;
+                document.getElementById('Estado').value = data.uf;
+
+                salvarDadosFormulario();
             }else{
-                alert("CEP INVALIDO")
-            }
+                alert("CEP INVALIDO");
+            };
 
         })
         .catch(error => console.error("Erro ao buscar CEP: ", error));
 
-})
+});
+
+AllInputs.forEach(element => {
+    element.addEventListener("blur", () => {
+        salvarDadosFormulario();
+    });
+}); 
 
 function mudarTema() {
     document.body.classList.toggle("light");
@@ -37,16 +76,27 @@ document.getElementById("mode").addEventListener("click", () => {
     const NovoTema = TemaAtual === "dark" ? "light" : "dark";
     localStorage.setItem("tema", NovoTema);
 
-    mudarTema()
+    mudarTema();
 
 });
 
 document.addEventListener("DOMContentLoaded", () => {
 
     const TemaSalvo = localStorage.getItem("tema");
+    const enderecoSalvo = localStorage.getItem("endereco");
 
     if (TemaSalvo === 'light') {
-        mudarTema()
-    }
+        mudarTema();
+    };
 
+    if (enderecoSalvo) {
+        carregarDadosFormulario(enderecoSalvo);
+    };
+});
+
+document.querySelector('#botaoEnvio').addEventListener("click", (evento) => {
+    evento.preventDefault();
+    
+    localStorage.removeItem("endereco");
+    window.location.href = "perro.html";
 });
